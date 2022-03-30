6ec3b53333e201498f440530f16b7450ec540819
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2022-03-31 01:37:50 +0200

    shift to random country idea

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const content = document.getElementById('travel-cards');
-    const search = document.getElementById('search');
-    const order = document.getElementById('order');
+    const content = document.querySelector('#travel-cards');
+    const search = document.querySelector('#search');
+    const order = document.querySelector('#order');
+    const info = document.querySelector('#info');
 
     // action buttons
-    const save = document.getElementById('save');
-    const load = document.getElementById('load');
-    const generate = document.getElementById('generate');
+    const saveSelection = document.querySelector('#save-selection');
+    const pickRandom = document.querySelector('#pick-random');
+    const clear = document.querySelector('#clear');
 
-    const actions = new DOMActions(content, [], search, order);
+    const actions = new DOMActions(content, search, order, info);
 
     search.addEventListener('keydown', event => {
         if (event.key == "Enter") {
@@ -18,9 +19,10 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    load.addEventListener('click', () => actions.load());
-    save.addEventListener('click', () => actions.save());
-    generate.addEventListener('click', () => actions.mock(data));
+    // load.addEventListener('click', () => actions.load());
+    saveSelection.addEventListener('click', () => actions.save());
+    pickRandom.addEventListener('click', () => actions.random());
+    clear.addEventListener('click', () => actions.clear());
 
     order.addEventListener('change', event => {
         actions.redraw(event.target.value);
--- utils/actions.js
+++ utils/actions.js
@@ -2,78 +2,109 @@
 
 class DOMActions {
 
-    static _sort(elements) {
-        const sorted = elements.sort((a, b) => a.title < b.title ? 1 : -1);
-        return sorted;
+    static _sort(elements, order) {
+        switch (order) {
+            case 'name-asc':
+                return elements.sort((a, b) => a.name > b.name ? 1 : -1);
+            case 'name-desc':
+                return elements.sort((a, b) => a.name < b.name ? 1 : -1);
+            case 'region-asc':
+                return elements.sort((a, b) => a.region > b.region ? 1 : -1);
+            case 'region-desc':
+                return elements.sort((a, b) => a.region < b.region ? 1 : -1);
+            default:
+                return elements;
+        }
     }
 
     static _filter(elements, query) {
-        return elements.filter(el => el.title.toLowerCase().includes(query));
+        return elements.filter(el => el.name.toLowerCase().includes(query));
     }
 
-    static _addIdentifiers(elements) {
-        return elements.map((el, index) => ({...el, identifier: `element_${index}`}));
+    static _getFromStorage() {
+        const data = localStorage.getItem('data');
+        return data !== null ? [true, JSON.parse(data)] : [false, []];
     }
 
-    constructor(content, elements, search, order) {
+    static async _fetchRandom() {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        const countries = await response.json();
+        const randomCountry = countries[Math.floor(Math.random() * countries.length)];
+        return {
+            name: randomCountry?.name?.common,
+            img: randomCountry?.flags?.png,
+            capital: randomCountry?.capital[0],
+            region: randomCountry?.region,
+            geolocation: randomCountry?.maps?.googleMaps
+        };
+    }
+
+    constructor(content, search, order, info) {
         this.content = content;
         this.search = search;
         this.order = order;
-        this.elements = elements;
+        this.elements = [];
+        this.info = info;
 
-        const [status, data] = this._getFromStorage();
+        const [status, data] = DOMActions._getFromStorage();
         if (status) this.elements = data;
         this.redraw();
     }
 
-    _createTravelCard({ img, title, description, identifier }) {
-        const card = document.createElement("div");
-        card.className = 'card';
+    _createTravelCard({ img, name, region, capital, geolocation, idx }) {
+        var div = document.createElement('div');
+        div.innerHTML = `
+            <div class='card'>
+                <img src='${img}'/>
+                <div class='card-content'>
+                    <div>
+                        <h1>${name}</h1>
+                        <hr/>
+                        ${('geolocation' in navigator) ?
+                            `<p><a target='_blank' href='${geolocation}'>Google maps for ${name}</a></p>`
+                            : ''
+                        }
+                        <p><a target='_blank' href='${geolocation}'>Flights to ${name}</a></p>
+                        <p>Capital: <b>${capital}</b></p>
+                        <p>Region: <b>${region}</b></p>
+                    </div>
+                    <div>
+                        <button class='del-button'>
+                            Delete destination
+                        </button>
+                    </div>
+                </div>
+            </div>
+        `.trim();
 
-        const imgEl = document.createElement("img");
-        imgEl.src = img;
-    
-        const titleEl = document.createElement("h1");
-        titleEl.textContent = title;
-    
-        const descriptionEl = document.createElement("p");
-        descriptionEl.textContent = description;
+        const card = div.firstChild;
+        const delButton = card.querySelector('button');
+        delButton.onclick = () => this.delete(idx);
 
-        const delButton = document.createElement('button');
-        delButton.onclick = () => this.delete(identifier);
-        delButton.textContent = 'delete';
-    
-        const cardContent = document.createElement("div");
-        cardContent.appendChild(titleEl);
-        cardContent.appendChild(descriptionEl);
-        cardContent.appendChild(delButton);
-    
-        card.appendChild(imgEl);
-        card.appendChild(cardContent);
-    
         return card;
     }
 
-    _getFromStorage() {
-        const data = localStorage.getItem('data');
-        return data !== null ? [true, JSON.parse(data)] : [false, []];        
+    _setInfo() {
+        this.info.textContent = `You have selected ${this.visible.length}/5 countries!`;
+        this.info.style.color = this.visible.length === 5 ? 'green' : 'red';
     }
 
     redraw() {
-        this.content.innerHTML = '';
-        this.elements = DOMActions._addIdentifiers(this.elements);
-
-        const sorted = DOMActions._sort(this.elements);
+        const sorted = DOMActions._sort(this.elements, this.order.value);
         const filtered = DOMActions._filter(sorted, this.search.value.toLowerCase());
 
-        for (const element of filtered) {
-            const card = this._createTravelCard(element);
+        this.content.innerHTML = '';
+        this.visible = filtered;
+        this._setInfo();
+
+        filtered.forEach((element, idx) => {
+            const card = this._createTravelCard({ ...element, idx });
             this.content.appendChild(card);
-        }
+        });
     }
 
-    delete(identifier) {
-        this.elements = this.elements.filter(el => el.identifier !== identifier);
+    delete(idx) {
+        this.elements.splice(idx, 1);
         this.redraw();
     }
 
@@ -82,16 +113,23 @@ class DOMActions {
         localStorage.setItem('data', str);
     }
 
-    load() {
-        const [status, data] = this._getFromStorage();
-        if (status) {
-            this.elements = data;
-            this.redraw();
-        }
+    clear() {
+        this.elements = [];
+        this.redraw();
     }
 
-    mock(data) {
-        this.elements = [...this.elements, ...data];
-        this.redraw();
+    async random() {
+        const loading = document.createElement('div');
+        loading.className = 'spinner';
+        this.content.prepend(loading);
+
+        while (true) {
+            const picked = await DOMActions._fetchRandom();
+            if (!this.elements.some(el => el.name === picked.name)) {
+                this.elements.push(picked);
+                this.redraw();
+                break;
+            }
+        }
     }
 }
